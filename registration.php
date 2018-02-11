ab0a7fc2c81f3d581104145bac3663795486b62e
Vladan Kuzmanovic <vladan.kuzman@gmail.com>
2018-02-11 08:55:10 +0100

    file comments

--- registration.php
+++ registration.php
@@ -1,6 +1,16 @@
 <?php
 /**
- * @author	    Vladan Kuzmanovic (vladankuzmanovic@gmail.com)
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Kuzman
+ * @package    Kuzman_AutoOrderCancel
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Vladan Kuzmanovic (vladan.kuzman@gmail.com)
  */
 use \Magento\Framework\Component\ComponentRegistrar;
 
